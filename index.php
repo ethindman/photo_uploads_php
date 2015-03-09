c3425fe990fa3350210e7cee772038be9ab5d0c2
Elliott Hindman <portokaliajapan@gmail.com>
2015-03-09 14:10:29 -0700

    Adds working version

---
+++ index.php
@@ -0,0 +1,5 @@
+<h2>Upload your image here!</h2>
+<form action="process.php" method="post" enctype="multipart/form-data">
+	<input type="file" name="fileToUpload">
+	<input type="submit" value="Submit">
+</form>
